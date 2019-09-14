42d34c32bbc8375c7386527f9aabd5d347c2946f
HeeyoonTak <wewe1003@naver.com>
2019-09-15 00:52:08 +0900

    quick

--- Baeckjoon/src/baekjoon2751/Main.java
+++ Baeckjoon/src/baekjoon2751/Main.java
@@ -5,18 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	static void swap(int[] quick, int i, int j) {
+		int temp = quick[j];
+		quick[j] = quick[i];
+		quick[i] = temp;
+	}
+	
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int[] quick = new int[Integer.parseInt(br.readLine())];
-		//베열에 저장
-		for(int i=0; i<quick.length; i++) {
-			quick[i]=Integer.parseInt(br.readLine());
+		// 베열에 저장
+		for (int i = 0; i < quick.length; i++) {
+			quick[i] = Integer.parseInt(br.readLine());
 		}
-		//정렬
-		for(int i=0; i<quick.length;i++) {
-			
+		// 정렬
+		for (int i = 0; i < quick.length; i++) {
+
 		}
 	}
 }
