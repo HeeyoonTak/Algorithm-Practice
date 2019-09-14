c02b805f03f6c686417e261806d1bf5deb87f418
HeeyoonTak <wewe1003@naver.com>
2019-09-14 22:33:05 +0900

    quick

--- Baeckjoon/src/baekjoon2751/Main.java
+++ Baeckjoon/src/baekjoon2751/Main.java
@@ -1,9 +1,18 @@
 package baekjoon2751;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] quick = new int[Integer.parseInt(br.readLine())];
+		//베열에 저장
+		for(int i=0; i<quick.length; i++) {
+			quick[i]=Integer.parseInt(br.readLine());
+		}
 	}
 }
