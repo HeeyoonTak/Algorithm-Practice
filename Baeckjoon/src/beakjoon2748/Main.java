550ad9029d2cff9db9f46dcc9aadebbc5094a710
HeeyoonTak <wewe1003@naver.com>
2019-09-22 23:53:06 +0900

    commit

--- Baeckjoon/src/beakjoon2748/Main.java
+++ Baeckjoon/src/beakjoon2748/Main.java
@@ -11,10 +11,10 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		long[] array = new long[n+1];
+		long[] array = new long[91];
 		array[0] = 0;
 		array[1] = 1;
-		for(int i=2;i<array.length;i++) {
+		for(int i=2;i<=n;i++) {
 			array[i]=array[i-2]+array[i-1];
 		}
 		System.out.println(array[n]);
