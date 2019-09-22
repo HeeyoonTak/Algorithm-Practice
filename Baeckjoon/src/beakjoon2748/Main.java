39f047156da2fd031f8c816c7ea20ec6528d9481
HeeyoonTak <wewe1003@naver.com>
2019-09-22 23:34:02 +0900

    fib dp

--- Baeckjoon/src/beakjoon2748/Main.java
+++ Baeckjoon/src/beakjoon2748/Main.java
@@ -11,14 +11,13 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] array = new int[n];
+		int[] array = new int[n+1];
 		array[0] = 0;
 		array[1] = 1;
-		int sum=0;
-		for(int i=0;i<array.length;i++) {
-			sum+=array[i];
+		for(int i=2;i<array.length;i++) {
+			array[i]=array[i-2]+array[i-1];
 		}
-		System.out.println(sum);
+		System.out.println(array[n]);
 	}
 
 }
