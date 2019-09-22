b16b28e10388701de598b832eb7bf2850c89b23f
HeeyoonTak <wewe1003@naver.com>
2019-09-22 23:15:20 +0900

    Fib Dp

---
+++ Baeckjoon/src/beakjoon2748/Main.java
@@ -0,0 +1,20 @@
+package beakjoon2748;
+
+//피보나치 DP
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] array = new int[n+1];
+		array[0] = 0;
+		array[1] = 1;
+		
+	}
+
+}
