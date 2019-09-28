06465446d4990bcc60e0e9f0baf8387245054480
HeeyoonTak <wewe1003@naver.com>
2019-09-28 22:06:08 +0900

    commit

--- Baeckjoon/src/baekjoon1427/Main.java
+++ Baeckjoon/src/baekjoon1427/Main.java
@@ -1,10 +1,26 @@
 package baekjoon1427;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 //sort inside
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] sort = new int[N];
+		for(int i=0; i<sort.length; i++) {
+			sort[i]=Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(sort);
+		for(int i=sort.length; i>-1; i--) {
+			System.out.println(sort[i]);
+		}
 	}
 
 }
