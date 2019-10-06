a474d2aa46a9b4fc6c3cdb92950fb10fbc12c696
HeeyoonTak <wewe1003@naver.com>
2019-10-06 23:44:13 +0900

    commit

--- Baeckjoon/src/baeckjoon11650/Main.java
+++ Baeckjoon/src/baeckjoon11650/Main.java
@@ -3,7 +3,8 @@ package baeckjoon11650;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Arrays;
+import java.util.Comparator;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -14,6 +15,21 @@ public class Main {
 		for(int i=0; i<location.length; i++) {
 			location[i][0]=Integer.parseInt(br.readLine());
 			location[i][0]=Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(location, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] x, int[] y) {
+				// TODO Auto-generated method stub
+				if(x[0] == y[0]) {
+					return Integer.compare(x[1], y[1]);
+				}
+				return Integer.compare(x[0], y[0]);
+			}
+			
+		});
+		for(int i=0; i<location.length; i++) {
+			System.out.println(location[i][0] + " " + location[i][1]);
 		}
 	}
 
