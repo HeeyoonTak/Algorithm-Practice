e286dc1431247711faaf762a70be58345e09032c
HeeyoonTak <wewe1003@naver.com>
2019-10-06 23:20:37 +0900

    commit

--- Baeckjoon/src/baeckjoon11650/Main.java
+++ Baeckjoon/src/baeckjoon11650/Main.java
@@ -9,7 +9,12 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		int N = Integer.parseInt(br.readLine());
+		int[][] location = new int[N][2];
+		for(int i=0; i<location.length; i++) {
+			location[i][0]=Integer.parseInt(br.readLine());
+			location[i][0]=Integer.parseInt(br.readLine());
+		}
 	}
 
 }
