119e284dc5fff661fd06bfddf43a35188c57956f
HeeyoonTak <wewe1003@naver.com>
2019-10-06 22:59:25 +0900

    commit

--- Baeckjoon/src/baeckjoon11650/Main.java
+++ Baeckjoon/src/baeckjoon11650/Main.java
@@ -1,9 +1,14 @@
 package baeckjoon11650;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 	}
 
