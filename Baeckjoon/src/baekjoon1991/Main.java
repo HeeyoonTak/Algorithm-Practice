94abdb0477f4908dfb7a5090eb58bae7fded931c
Heeyoon <wewe1003@naver.com>
2020-01-13 21:50:45 +0900

    tree order

--- Baeckjoon/src/baekjoon1991/Main.java
+++ Baeckjoon/src/baekjoon1991/Main.java
@@ -1,5 +1,29 @@
 package baekjoon1991;
 
+//트리순회
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Main {
 
+	class Node{
+		char data;
+		Node left,right;
+		public Node(char data) {
+			super();
+			this.data = data;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		
+		int n= Integer.parseInt(br.readLine());
+		
+	}
+	
 }
