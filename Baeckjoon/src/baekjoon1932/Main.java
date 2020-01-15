a991adecbfe2ad2a07fbe85619d6145cd2cf62f2
Heeyoon <wewe1003@naver.com>
2020-01-15 15:35:42 +0900

    IntTriangleDP

--- Baeckjoon/src/baekjoon1932/Main.java
+++ Baeckjoon/src/baekjoon1932/Main.java
@@ -8,12 +8,17 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		
-		int array[][]=new int[n][];
+		int sum = 0;
+		int array[][] = new int[n][];
 
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				array[i][j] = Integer.parseInt(br.readLine());
+			}
+		}
 	}
 
 }
