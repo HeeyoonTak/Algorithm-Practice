63b050f229800d398acc6b15e9bf7947eaeac506
Heeyoon <wewe1003@naver.com>
2020-01-15 15:17:52 +0900

    IntTriangleDP

---
+++ Baeckjoon/src/baekjoon1932/Main.java
@@ -0,0 +1,19 @@
+package baekjoon1932;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int array[][]=new int[n][];
+
+	}
+
+}
