db6f05b93535a09cdd70cd529273e9e3049a24a9
Heeyoon <wewe1003@naver.com>
2020-01-15 17:32:59 +0900

    Chocolate DP

---
+++ Baeckjoon/src/baekjoon2163/Main.java
@@ -0,0 +1,16 @@
+package baekjoon2163;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int array[][]=new int[N][M];
+	}
+
+}
