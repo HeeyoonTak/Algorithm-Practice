e22b7c6b4f362d1932529901a581b3c51a2eb82f
Heeyoon <wewe1003@naver.com>
2020-01-15 17:19:52 +0900

    IntTriangleDP

--- Baeckjoon/src/baekjoon1932/Main.java
+++ Baeckjoon/src/baekjoon1932/Main.java
@@ -1,22 +1,26 @@
 package baekjoon1932;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
+//		int n = Integer.parseInt(br.readLine());
+		int n=sc.nextInt();
 		int sum = 0;
 		int array[][] = new int[n + 1][n + 1];
-
+		array[0][0]=0;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
-				array[i][j] = Integer.parseInt(br.readLine());
+//				array[i][j] = Integer.parseInt(br.readLine());
+				array[i][j]=sc.nextInt();
 				if (j == 1) {
 					array[i][j] = array[i][j] + array[i - 1][j];
 				} else if (i == j) {
