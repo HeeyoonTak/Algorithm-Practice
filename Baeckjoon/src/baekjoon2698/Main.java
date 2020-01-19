8e110edd3226339bf08e3b4b9707cd55bd39e761
'Heeyoon <'wewe1003@naver.com'>
2020-01-19 17:27:49 +0900

    문제 이해 실패 -- 추후 다시 경건한 마음으로...

--- Baeckjoon/src/baekjoon2698/Main.java
+++ Baeckjoon/src/baekjoon2698/Main.java
@@ -1,8 +1,11 @@
 package baekjoon2698;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 //인접비트 DP
 public class Main {
@@ -11,7 +14,11 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));;
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[][][] array= new int[101][100][2];
 		
 	}
 
