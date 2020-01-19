f060bba953929b66cd5ce6f9f7715b7a16ef4843
'Heeyoon <'wewe1003@naver.com'>
2020-01-19 14:36:45 +0900

    인접비트 DP

---
+++ Baeckjoon/src/baekjoon2698/Main.java
@@ -0,0 +1,18 @@
+package baekjoon2698;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//인접비트 DP
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+	}
+
+}
