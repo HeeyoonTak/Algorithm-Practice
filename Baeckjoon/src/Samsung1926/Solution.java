1e3308223e9dafda2488afd26d9366e9492ea52b
Heeyoon <wewe1003@naver.com>
2020-01-30 23:45:39 +0900

    SWEA 1926 간단한 369게임 -  후에 다시 풀기

---
+++ Baeckjoon/src/Samsung1926/Solution.java
@@ -0,0 +1,25 @@
+package Samsung1926;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N;
+		N = sc.nextInt();
+		
+		for (int test_case = 1; test_case <= N; test_case++) {
+
+			/////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+			 * 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+			 */
+			/////////////////////////////////////////////////////////////////////////////////////////////
+
+		}
+
+	}
+
+}
