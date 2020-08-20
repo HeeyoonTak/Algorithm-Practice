d0fd1968fa462221a41aa98281b5956ff2e7b1f1
'Heeyoon <wewe1003@naver.com>
2020-08-20 12:59:45 +0900

    1325 - 효율적인 해킹(완료 X - dp 더 공부하고 다시 풀이)

---
+++ Baeckjoon/src/baekjoon1325/Main.java
@@ -0,0 +1,39 @@
+package baekjoon1325;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static ArrayList<Integer>[] list;
+	static boolean[] visited;
+	static int N, M;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		list = new ArrayList[N + 1];
+		visited = new boolean[N + 1];
+		for(int i = 1; i<=N; i++) {
+			list[i] = new ArrayList<Integer>();
+		}
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[b].add(a);
+		}
+		
+	}
+
+}
